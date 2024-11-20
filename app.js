1c882996fe102168620deafd093e2aa16b718f3b
worsleyj <worsleyj23@gmail.com>
2024-11-20 16:36:48 -0500

    add basic router

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const router = require("./routes/router");
+
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use("/", router);
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
