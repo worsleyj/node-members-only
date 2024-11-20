c8116eb4793334d8f7a2b9216bba609908673137
worsleyj <worsleyj23@gmail.com>
2024-11-20 16:02:46 -0500

    add app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`members only message board listening on port ${PORT}`)
+);
