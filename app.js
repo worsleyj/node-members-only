a12d0bb4854ebbe1513d1b25034a074122c4c83b
worsleyj <worsleyj23@gmail.com>
2024-11-20 16:58:02 -0500

    add stylesheet to ejs views

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const router = require("./routes/router");
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use("/", router);
 
