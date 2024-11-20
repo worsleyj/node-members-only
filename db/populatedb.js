1cdc0435b96f07fa10286cad56c6de4683cd7383
worsleyj <worsleyj23@gmail.com>
2024-11-20 16:25:39 -0500

    add db populate script for users and messages

---
+++ db/populatedb.js
@@ -0,0 +1,55 @@
+const { Client } = require("pg");
+
+// const SQL = `
+// CREATE TABLE IF NOT EXISTS usernames (
+//   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+//   username VARCHAR ( 255 )
+// );
+
+// INSERT INTO usernames (username)
+// VALUES
+//   ('Bryan'),
+//   ('Odin'),
+//   ('Damon');
+// `;
+
+const SQL = `
+CREATE DATABASE members_only;
+
+CREATE TABLE IF NOT EXISTS users (
+id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+first_name varchar(255),
+last_name varchar(255),
+username varchar(255),
+password varchar(255),
+member_status varchar(255));
+
+INSERT INTO users (first_name, last_name, username, password, member_status) 
+VALUES 
+('john', 'doe', 'jdoe@gmail.com', 'pass', 'member');
+
+CREATE TABLE IF NOT EXISTS messages (
+id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+title varchar(255),
+timestamp TIMESTAMP, 
+text varchar(255),
+created_by varchar(255));
+
+INSERT INTO messages (title, timestamp, text, created_by) 
+VALUES 
+('sample text', '', 'bottom text', 'john';)
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString:
+      "postgresql://postgres:postgres@localhost:5432/computer_parts",
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
