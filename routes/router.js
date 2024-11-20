64f4ca8ebfceedd712d3a93674ff160fbb2a9db3
worsleyj <worsleyj23@gmail.com>
2024-11-20 16:47:00 -0500

    add signup form and routing

--- routes/router.js
+++ routes/router.js
@@ -4,5 +4,7 @@ const router = Router();
 router.get("/", (req, res) => {
   res.render("index");
 });
-
+router.get("/sign-up", (req, res) => {
+  res.render("sign-up");
+});
 module.exports = router;
